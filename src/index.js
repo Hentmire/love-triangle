72abe53358997b24e8ea819f5775f783e6de13c1
Victoria Udodava <43163503+Hentmire@users.noreply.github.com>
2018-09-22 16:12:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var loveTriangles = 0;
+ 	for (var i = 1; i <= preferences.length; i++) {
+	  	var firstlover = preferences[i-1];
+	  	if (firstlover != i) {  // если первый чел не любит сам себя 
+	  		var secondLover = preferences[firstlover-1];
+	  		if (secondLover != firstlover && secondLover != i) { // если второй чел не любит сам себя и не любит первого чела
+	  			if (preferences[secondLover-1] == i) { // если третий чел любит первого чела
+	  				loveTriangles += 1;
+	  			}
+	  		}
+	  	}
+    }
+    return (loveTriangles / 3);
 };
